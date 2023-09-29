d55b10038f438bd37b7aa9994a8bafb1c8088e8c
Ashwini Kumar Pandey <ashwini.pandey@weverve.com>
2023-09-29 23:10:26 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT;
+
+app.get("/",(req,res)=>{
+    res.send("<h1>This is Server</h1>");
+});
+
+app.get('/sample', (req,res)=>{
+    try {
+        res.send("<h2>Sample route for test</h2>")
+    } catch (error) {
+       res.render(error);
+    }
+    
+});
+
+app.get('/login', (req,res)=>{
+    try {
+        res.status(200).json('this is the login page')
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server started on port ${port}`);  //localhost:3000
+});
